3af3ef8022ffcaddad8fd9115e9fd5c7317bf503
SophiesMom <YourEmail>
2020-02-29 15:13:11 -0500

    update CancelReservation.java

--- src/test/CancelReservation.java
+++ src/test/CancelReservation.java
@@ -3,5 +3,6 @@ package test;
 public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("Reservation cancelled");
+        System.out.println("Changes");
     }
 }
