bb2a2ea7fd3ddf9b0bb8c98d497acf922cfa4999
Cigdem Ertekin <58123554+SophiesMom4@users.noreply.github.com>
2020-02-29 15:14:14 -0500

    Update CancelReservation.java

--- src/test/CancelReservation.java
+++ src/test/CancelReservation.java
@@ -4,5 +4,7 @@ public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("Reservation cancelled");
         System.out.println("Changes");
+        System.out.println("Changes from GITHUB");
+        
     }
 }
